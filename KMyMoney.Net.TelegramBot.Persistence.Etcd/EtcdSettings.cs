90276f7950f3cfa7e3a403972f6db3bfe7c40cff
Ilia Ershov <filejunkie@gmail.com>
2025-08-03 14:51:46 +0200

    14 use etcd instead of in memory storage (#24)
    * ETCD connection works
    
    * Back to using certificate pair
    
    * Sane certificate setup

---
+++ KMyMoney.Net.TelegramBot.Persistence.Etcd/EtcdSettings.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KMyMoney.Net.TelegramBot.Persistence.Etcd;
+
+public class EtcdSettings
+{
+    [Required]
+    public required string ConnectionString { get; init; }
+    [Required]
+    public required string RootCertificate { get; init; }
+    [Required]
+    public required string Certificate { get; init; }
+    [Required]
+    public required string Key { get; init; }
+}
--- KMyMoney.Net.TelegramBot.Persistence.Etcd/ServiceConfigurationExtensions.cs
+++ KMyMoney.Net.TelegramBot.Persistence.Etcd/ServiceConfigurationExtensions.cs
@@ -1,26 +1,63 @@
-using dotnet_etcd.DependencyInjection;
+using System.Net.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using dotnet_etcd;
+using dotnet_etcd.interfaces;
 using Grpc.Core;
+using Grpc.Net.Client;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace KMyMoney.Net.TelegramBot.Persistence.Etcd;
 
 public static class ServiceConfigurationExtensions
 {
-    public static IServiceCollection AddEtcdPersistenceLayer(this IServiceCollection services)
-    {
-        return services.AddEtcdClient(options =>
+    public static IServiceCollection AddEtcdPersistenceLayer(this IServiceCollection services) => services
+        .AddSingleton<IEtcdClient>(sp =>
         {
-            // TODO use options
-            options.ConnectionString = Environment.GetEnvironmentVariable("ETCD_CONNECTION_STRING")!;
+            var settings = sp.GetRequiredService<IOptions<EtcdSettings>>().Value;
+            using var certificate = X509CertificateLoader.LoadCertificateFromFile(settings.Certificate);
+            using var key = RSA.Create();
+            key.ImportFromPem(File.ReadAllText(settings.Key));
+            var certificatePair = certificate.CopyWithPrivateKey(key);
+            var clientCertificateCollection = new X509CertificateCollection { certificatePair };
+            var handler = new SocketsHttpHandler
+            {
+                KeepAlivePingDelay = TimeSpan.FromSeconds(30),
+                KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
+                KeepAlivePingPolicy = HttpKeepAlivePingPolicy.Always,
+                SslOptions = new()
+                {
+                    ClientCertificates = clientCertificateCollection,
+                    RemoteCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
+                    {
+                        if (sslPolicyErrors == SslPolicyErrors.None)
+                        {
+                            return true;
+                        }
 
-            options.ConfigureChannel = channelOptions => {
-                channelOptions.Credentials = new SslCredentials(
-                    rootCertificates: File.ReadAllText(Environment.GetEnvironmentVariable("ETCD_CACERT")!),
-                    keyCertificatePair: new KeyCertificatePair(
-                        File.ReadAllText(Environment.GetEnvironmentVariable("ETCD_CERT")!),
-                        File.ReadAllText(Environment.GetEnvironmentVariable("ETCD_KEY")!)
-                    ));
+                        if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateChainErrors) == 0
+                            || chain == null
+                            || cert == null)
+                        {
+                            return false;
+                        }
+
+                        var rootCert = X509CertificateLoader.LoadCertificateFromFile(settings.RootCertificate);
+                        chain.ChainPolicy.CustomTrustStore.Add(rootCert);
+                        chain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
+                        return chain.Build(new X509Certificate2(cert));
+                    }
+                }
             };
-        });
-    }
+
+            return new EtcdClient(connectionString:
+                settings.ConnectionString,
+                configureChannelOptions: opts =>
+                {
+                    opts.Credentials = ChannelCredentials.SecureSsl;
+                    opts.HttpHandler = handler;
+                });
+        })
+        .AddSingleton<ISettingsPersistenceLayer, EtcdSettingsPersistenceLayer>();
 }
